9c4070da94bd6d583b62d8718f45971e7fe735d7
KentTain <tianchangjun@outlook.com>
2019-03-19 08:43:56 +0800

    Create Project

---
+++ src/main/java/com/two/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.two.dao;
+
+import com.two.entity.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer userId);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer userId);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
