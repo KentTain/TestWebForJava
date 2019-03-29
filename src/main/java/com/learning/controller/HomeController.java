0dbdba4815fcc280574ccf3936a9f98b3cdc990b
KentTain <tianchangjun@outlook.com>
2019-03-29 18:00:18 +0800

    三种srping servlets配置

--- src/main/java/com/learning/controller/HomeController.java
+++ src/main/java/com/learning/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.learning.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,11 +9,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
 
 @Controller
 @RequestMapping("/home")
 ///home/**
-public class HomeController {
+public class HomeController extends AbstractController{
 	private static Logger log = LoggerFactory.getLogger(HomeController.class);
 
 	// /home/index
@@ -20,5 +23,12 @@ public class HomeController {
 	public String home(HttpServletRequest request, Model model) {
 		log.debug("/home/index");
 		return "index";
+	}
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
